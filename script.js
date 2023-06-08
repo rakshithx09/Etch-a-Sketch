03b93a1f65fa5bce25ffcfef5ec47cf054519da3
Rakshith <rakshithx09@gmail.com>
2023-06-08 22:08:01 +0530

    input added

--- script.js
+++ script.js
@@ -1,55 +1,49 @@
 let num = 0, divwidth = 0, boxDimensions = 0, boxesAll;
 
+
+function initializeNum(){
+    num=0
+    const input=document.getElementById('input');
+    num=Number(input.value) ;
+    createBoxes()
+}
+
 function createBoxes() {
 
     const canvas = document.getElementById('mainCanvas');
     console.log(canvas);
-    num = 120;
+    //num = 90;
     boxDimensions = 30 / num;
     console.log(boxDimensions);
+    
+    //creating box and gicing it a class
     for (let i = 0; i < num * num; i++) {
-
         let box = document.createElement('div');
         canvas.appendChild(box);
         box.setAttribute('class', 'customBoxes');
-        
-        
     }
+
+    //getting reference to all boxes
     boxesAll = document.querySelectorAll('.customBoxes');
+    
+    //setting dimensions of box acc to canvas size
     boxesAll.forEach(boxes => {
-        
+
         boxes.style.cssText = ` background-color: beige ; width: ${30 / num}rem; height: ${30 / num}rem; box-sizing: border-box;`;
-        
+
+        //adding hover effect
         boxes.addEventListener('mouseenter', () => {
-            boxes.setAttribute('id', 'toggle') ;
+            boxes.setAttribute('id', 'toggle');
             //boxes.classList.toggle('toggle');
             setTimeout(() => {
                 console.log("timeout")
-                boxes.removeAttribute('id') ;
+                boxes.removeAttribute('id');
                 //boxes.classList.toggle('toggle');
-            }, 200)
+            }, 1600)
         });
-        
-        /*boxes.addEventListener('mouseleave', () => {
-            boxes.removeAttribute('id') ;
-            //boxes.classList.toggle('toggle');
-        });
-
-        */
-
-
     });
-   /* for (let boxes of boxesAll) {
-        boxes.addEventListener('onclick', () => {
-            boxes.classList.toggle('toggle');
-        })
-        boxes.addEventListener('mouseleave', () => {
-            boxes.classList.toggle('toggle');
-        })
-    }*/
-
 }
 
 
 
-createBoxes();
+//createBoxes();
